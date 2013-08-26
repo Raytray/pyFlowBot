6d0aed4bca07e7f5b5bc8b1b0094e072dd7bef6e
Afzal Jasani <amj355@nyu.edu>
2013-08-26 12:38:43 -0400

    Stream from multiple flows/ match word in dict

--- pyflowbot.py
+++ pyflowbot.py
@@ -10,9 +10,13 @@ config.read('apikey.cfg')
 
 FLOW_USER_API_KEY = config.get('DEFAULT', 'FLOW_USER_API_KEY')
 FLOW_TOKEN = config.get('DEFAULT' , 'FLOW_TOKEN' )
+match_word = {
+        "!catgif": "get_gif()",
+        "!doggif": "get_gif()"
+    }
 
 CAT_URL = "http://thecatapi.com/api/images/get?format=src&type=gif"
-
+Flows_to_fetch = ['parsely/eng-interns', 'parsely/devtalk', 'parsely/parse-ly-sales', 'parsely/main']
 
 def get_gif():
     gif_url = requests.get(CAT_URL)
@@ -20,22 +24,20 @@ def get_gif():
 
 
 def process_data(data):
-    matching_word = "!catgif"
-    data_of_flow = data["content"]
-    if data_of_flow.find("!catgif") != -1:
-        send_gif(get_gif())
-
-
+    if data['content'] in match_word:
+        print data['flow']
+        send_gif(eval(match_word[data['content']]))
+        
 def send_gif(gif_url):
-   chat = Chat(FLOW_TOKEN)
-   chat.post(gif_url, 'PyFlowBot')
+    chat = Chat(FLOW_TOKEN)
+    chat.post(gif_url, 'PyFlowBot')
 
 
 def main():
     stream = JSONStream(FLOW_USER_API_KEY)
-    gen = stream.fetch(['parsely/eng-interns'])
+    gen = stream.fetch(Flows_to_fetch)
     for data in gen:
-        if type(data) == dict and data["event"] == "message":
+        if type(data) == dict and data['event'] == "message":
             process_data(data)
 
 
